337086874aa5d384ef04192477ce3a8876cec227
Poppa Rocks <leon.johnson@gmail.com>
2018-05-12 12:06:20 -0400

    :bug: ranks rapper, order wrong
    sends back the least wackest rapper first

--- rapper.go
+++ rapper.go
@@ -3,7 +3,9 @@
 package wackrapper
 
 import (
+	"fmt"
 	"math"
+	"sort"
 
 	"github.com/rs/xid"
 )
@@ -16,7 +18,7 @@ type Rapper struct {
 	lyrics   []lyric
 }
 
-func (r *Rapper) Wackness() float64 {
+func (r Rapper) Wackness() float64 {
 	var total float64 = 0
 	for _, v := range r.lyrics {
 		total += v.wackness
@@ -31,4 +33,65 @@ func (r *Rapper) Wackness() float64 {
 // New returns a new rapper
 func New() Rapper {
 	return Rapper{xid: xid.New()}
+}
+
+// Rank returns an ordered array of rappers in order
+// of wackness. The first rapper is the wackest in
+// the group.
+func Rank(rr []Rapper) []string {
+
+	type ranking struct {
+		name     string
+		wackness float64
+	}
+
+	switch len(rr) {
+	case 0:
+		return []string{}
+	default:
+		m := make(map[int]Rapper)
+		// Load map with rappers
+		for k, v := range rr {
+			m[k] = v
+		}
+
+		// Load map keys into a slice
+		keys := []float64{}
+		for i := range m {
+			keys = append(keys, m[i].Wackness())
+		}
+		// Sort slice
+		//sort.Sort(sort.Reverse(sort.Float64Slice(keys)))
+		//orderedKeys := make([]float64, len(keys))
+		//for i, j := 0, len(keys)-1; i < j; i, j = i+1, j-1 {
+		//orderedKeys[i], orderedKeys[j] = keys[j], keys[i]
+		//}
+		for i := len(keys)/2 - 1; i >= 0; i-- {
+			opp := len(keys) - 1 - i
+			keys[i], keys[opp] = keys[opp], keys[i]
+		}
+		sort.Sort(sort.Reverse(sort.Float64Slice(keys)))
+		fmt.Printf("%v\n", keys)
+		// Build slice of strings based on map now that
+		// values are in order
+		ss := []string{}
+		for k := range keys {
+			ss = append(ss, m[k].name)
+		}
+		fmt.Printf("%v\n", ss)
+		/*
+			rr := []ranking{}
+			for k := range keys {
+				rr = append(rr, ranking{m[k].name, m[k].Wackness()})
+			}
+
+			ss := []string{}
+			for _, v := range rr {
+				//ss = append(ss, v.name, strconv.FormatFloat(v.wackness, 'f', 2, 64))
+				ss = append(ss, v.name)
+			}*/
+
+		return ss
+	}
+
 }
--- rapper_test.go
+++ rapper_test.go
@@ -1,7 +1,6 @@
 package wackrapper
 
 import (
-	"fmt"
 	"math"
 	"testing"
 )
@@ -14,12 +13,53 @@ func TestNew(t *testing.T) {
 	// TODO: Add better test
 }
 
+func benchmarkNew(i int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		if n < i {
+			New()
+		}
+	}
+}
+
+func BenchmarkNew10(b *testing.B)         { benchmarkNew(10, b) }
+func BenchmarkNew1000(b *testing.B)       { benchmarkNew(1000, b) }
+func BenchmarkNew1000000(b *testing.B)    { benchmarkNew(1000000, b) }
+func BenchmarkNew1000000000(b *testing.B) { benchmarkNew(1000000000, b) }
+
 func TestWackness(t *testing.T) {
 	for _, tc := range testWackRappers {
 		w := tc.rapper.Wackness()
-		fmt.Printf("%v's wackness: %v\n", tc.rapper.name, w)
+		//fmt.Printf("%v's wackness: %v\n", tc.rapper.name, w)
 		if math.IsNaN(w) {
 			t.Errorf("FAIL %v - got %v want any number", tc.description, w)
 		}
 	}
+}
+
+func TestRank(t *testing.T) {
+	want := []string{"Canibus", "Nicki Minaj", "Chuggo", "Lil Invisible"}
+	rr := []Rapper{}
+	//for _, tc := range testWackRappers {
+	//rr :=
+	//}
+	empty := Rank(rr)
+	if len(empty) != 0 {
+		t.Errorf("FAIL wrong lenth list; want 0 got %v", len(empty))
+	}
+
+	for _, tc := range testWackRappers {
+		rr = append(rr, tc.rapper)
+	}
+	list := Rank(rr)
+	switch {
+	case len(list) != len(want):
+		t.Errorf("FAIL got wrong lenth Rank() response want %v got %v", len(want), len(list))
+	case len(list) == len(want):
+		for i := range list {
+			if list[i] != want[i] {
+				t.Errorf("FAIL got wrong order; want %v got %v", want[i], list[i])
+			}
+		}
+	}
+
 }
